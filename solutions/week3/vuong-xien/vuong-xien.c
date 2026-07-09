9998b589d2e9d7495b1c99682779e0a25efaf573
deanveb <Annhi1975@gmail.com>
2026-07-09 16:14:45 +0700

    optimized vuong-xien

--- solutions/week3/vuong-xien/vuong-xien.c
+++ solutions/week3/vuong-xien/vuong-xien.c
@@ -1,9 +1,6 @@
 #include "cs50.h"
 #include "stdio.h"
 
-int printDiagonal(int size, int y, int x, int numberToPrint,
-                  int buffer[size][size]);
-
 int main(void) {
   int n = 0;
   do {
@@ -17,36 +14,18 @@ int main(void) {
       buffer[i][j] = 0;
     }
   }
-  int count = 1;
+
   for (int i = 0; i < n; i++) {
-    if (printDiagonal(n, i, 0, count, buffer) == -1)
-      return 1;
-    count++;
+    for (int j = 0, value = i + 1; j < n; j++, value++) {
+      buffer[i][j] = value;
+    }
   }
 
-  for (int j = 1; j < n; j++) {
-    if (printDiagonal(n, n - 1, j, count, buffer) == -1)
-      return 1;
-    count++;
-  }
-
+  // Print result
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < n; j++) {
       printf("%d", buffer[i][j]);
     }
     printf("\n");
   }
-}
-
-int printDiagonal(int size, int y, int x, int numberToPrint,
-                  int buffer[size][size]) {
-  // if (y < 0 || x < 0 || y >= size || x >= size)
-  //   return -1;
-  do {
-    buffer[y][x] = numberToPrint;
-    y--;
-    x++;
-  } while (y >= 0 && x < size);
-
-  return 0;
 }
