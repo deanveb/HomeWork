1cbcdd03ee195ddc7861105c3495447f15112ced
deanveb <Annhi1975@gmail.com>
2026-07-14 14:57:38 +0700

    fixed spacing issue

--- solutions/week3/tam-giac-pascal/tam-giac-pascal.c
+++ solutions/week3/tam-giac-pascal/tam-giac-pascal.c
@@ -30,7 +30,7 @@ int main(void) {
 
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < i + 1; j++) {
-      printf("%d ", buffer[i][j]);
+      printf((j + 1 >= i + 1) ? "%d" : "%d ", buffer[i][j]);
     }
     printf("\n");
   }
